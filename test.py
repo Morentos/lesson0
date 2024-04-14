7605a6eccfc08bd714dff74b424855b58cfd5fb4
Morentos <166997688+Morentos@users.noreply.github.com>
2024-04-14 15:23:22 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+print(5 ** 2) # // - целочисленное деление, / - деление, % - остаток при делении, ** - возведение в степень
+print(type("Hello, world")) # int- integer,
+print('Hello, ' + 'world!') # concatenate - сложение строк
+print(True, False) # boolean
+print(5<5, 10>=5, 5+5, 'hello, world') # == - равенство, != - неравенство
+print( 5 != 5 or 5 < 10)
+print(type(int('5'))) # иизменение типа данных
